0e4d3309b7b414670d53517de3d6530543ebadb8
SemyonPolyotin <catsemen@yandex.ru>
2020-06-09 02:34:00 +0300

    Style - Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+//no using namespace here
 
-int main(){
+int
+main()
+{
   std::string name;
   std::cin >> name;
-  std::cout << "Hello world from " << name << std::endl;
+  std::cout << "Hello world from " << name
+            << std::endl;
   return 0;
 }
+//more simple text
